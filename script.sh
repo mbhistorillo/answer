4f2a0b44e06c3566cf7e5f1f5226a0c9ee2b846a
mbhistorillo <mbmhistorillo@gmail.com>
2017-01-26 21:21:03 +0800

    added starting count of all http code
    added code to display the starting count of all present http code when the script was executed

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
  #!/bin/sh
 
 log=$1
-echo "Starting"
+#display starting count of all http code present when the file was executed
+echo "=======Starting"
+cat $log | awk  '{print $9}' | sort | uniq -c | sort -nr | awk '{print "http code " $2 ": " $1}'
 # Check if file was changed
 while  S=$(inotifywait -eMODIFY $log 2>/dev/null)
 do
-echo "============"
+echo "======="
  # if it was changed sort and count each unique http code
 cat $log | awk  '{print $9}' | sort | uniq -c | sort -nr | awk '{print "http code " $2 ": " $1}'
 done
